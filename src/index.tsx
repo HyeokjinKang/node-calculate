ed76a51f938bb7e335d84893247095be12f8fbbf
HyeokjinKang <coupy@croissant.tech>
2020-12-21 17:41:11 +0900

    initialize for build

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
 import { Text, Window, hot, View, Renderer } from "@nodegui/react-nodegui";
 import React from "react";
 
+const minSize = { width: 500, height: 600 };
+
 const App = () => {
   return (
-    <Window>
-
+    <Window styleSheet={styleSheet} minSize={minSize} windowTitle="Calculator">
+      <View id="rootView">
+        <View id="label"></View>
+        <View id="calBtn"></View>
+        <View id="calBtn"></View>
+        <View id="calBtn"></View>
+      </View>
     </Window>
   );
 };
+
+const styleSheet = `
+  #rootView{
+    height: '100%';
+    background-color: blue;
+  }
+
+  #label {
+    background-color: white;
+    flex: 1;
+  }
+
+  #calBtn {
+    background-color: #222;
+    flex: 1;
+  }
+`;
 
 Renderer.render(<App />);
