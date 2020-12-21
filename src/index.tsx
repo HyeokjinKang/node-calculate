cdfdfcca4e15a735e2ff4201d5c792281141ee3a
HyeokjinKang <coupy@croissant.tech>
2020-12-21 17:29:04 +0900

    initialize

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { Text, Window, hot, View, Renderer } from "@nodegui/react-nodegui";
+import React from "react";
+
+const App = () => {
+  return (
+    <Window>
+
+    </Window>
+  );
+};
+
+Renderer.render(<App />);
